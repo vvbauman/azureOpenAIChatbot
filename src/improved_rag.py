99f1c0d09ebe6fbf02a8103be9b4c9765320a0dd
Val Bauman <valerie.bauman@teck.com>
2024-07-29 15:13:30 -0400

    clean up improved rag

--- src/improved_rag.py
+++ src/improved_rag.py
@@ -87,14 +87,9 @@ def get_search_query(chat_completion: ChatCompletion, user_query: str):
 def main():
     # get OpenAI client and specify some chat completion parameters, same as before
     oai_client, search_client = get_config()
-    system_message1 = """Assistant helps the company employees with their healthcare plan questions, and questions about the employee handbook. Be brief in your answers.
-        Answer ONLY with the facts listed in the list of sources below. If there isn't enough information below, say you don't know. 
-        Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question.
-        Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brackets to reference the source, for example [info1.txt]. 
-        Don't combine sources, list each source separately, for example [info1.txt][info2.pdf].
-        """
-    system_message = """You are an assistant that summarizes document highlights retrieved from documents using Azure AI Search. Your responses should be 2-3 sentences and should include
-        all key details without adding external information or assumptions.
+    system_message = """You are an assistant that summarizes document highlights retrieved from documents using Azure AI Search. You will receive the search query in double asterisks, for example, **eye exams**.
+        You should start all responses with "this is what I know about **search query**". For example, for search query **eye exams**, your response will start as "This is what I know about eye exams".
+        Your responses should be 2-3 sentences and should include all key details without adding external information or assumptions. 
         """
     temperature = 0.3 # response creativity (0-2, 0 being entirely factual and literal)
     max_tokens = 1000 # repsonse token limit. 1 token ~= 4 characters
@@ -102,9 +97,8 @@ def main():
 
     # get the token limit for the model we've deployed - see https://github.com/pamelafox/openai-messages-token-helper/blob/main/src/openai_messages_token_helper/model_helper.py
     deployment_name = os.getenv("OAI_DEPLOYMENT")
-    model_name = "gpt-3.5-turbo"
+    model_name = os.getenv("MODEL_NAME")
     model_token_limit = get_token_limit(model=model_name)
-    print("DEPLOYMENT", deployment_name)
 
     # create the prompt we'll use to create the optimized search query
     query_prompt_template = """Below is a history of the conversation so far, and a new question asked by the user that needs to be answered by searching in a knowledge base.
@@ -115,9 +109,7 @@ def main():
         Do not include any special characters like '+'.
         If you cannot generate a search query, return just the number 0.
         """   
-    summarize_prompt_template = """Summarize the following text into 2-3 sentences, ensuring to include all key details without 
-        adding any external information or assumptions.
-        """
+    
     query_resp_token_limit = 100 # max tokens to create optimized search query
     
     # define tools used to build messages to get optimized search query - see https://platform.openai.com/docs/api-reference/chat/create#chat-create-tools
@@ -150,28 +142,26 @@ def main():
 
         # STEP 2) Create an optimized search query from the user input
         # build messages to send to model to get search query - see https://github.com/pamelafox/openai-messages-token-helper/blob/main/src/openai_messages_token_helper/message_builder.py
-        # query_messages = build_messages(
-        #     model = model_name, # need openAI-friendly name here
-        #     system_prompt = query_prompt_template,
-        #     tools = tools,
-        #     past_messages = [] if q == 0 else messages[:-1], # TODO: figure out how this works for the first turn
-        #     # user_request_query = "Generate search query for: " + text, # not a valid argument for this method
-        #     new_user_content = "Generate search query for: " + text,
-        #     max_tokens = model_token_limit - query_resp_token_limit,
-        # )       
-        # print("QUERY MESSAGES:", query_messages)
+        query_messages = build_messages(
+            model = model_name, # need openAI-friendly name here
+            system_prompt = query_prompt_template,
+            tools = tools,
+            past_messages = [] if q == 0 else messages[:-1], # TODO: figure out how this works for the first turn
+            # user_request_query = "Generate search query for: " + text, # not a valid argument for this method
+            new_user_content = "Generate search query for: " + text,
+            max_tokens = model_token_limit - query_resp_token_limit,
+        )       
         # send the messages to the Azure OpenAI client to create the optimized search query
-        # chat_completion : ChatCompletion = oai_client.chat.completions.create(
-        #     messages = query_messages,
-        #     model = deployment_name,
-        #     temperature = 0, # minimize creativity for search query creation
-        #     max_tokens = query_resp_token_limit,
-        #     n = 1,
-        #     tools = tools,
-        # )
+        chat_completion : ChatCompletion = oai_client.chat.completions.create(
+            messages = query_messages,
+            model = deployment_name,
+            temperature = 0, # minimize creativity for search query creation
+            max_tokens = query_resp_token_limit,
+            n = 1,
+            tools = tools,
+        )
 
-        query_text = "eye exam" #get_search_query(chat_completion=chat_completion, user_query=text)
-        print("QUERY TEXT", query_text)
+        query_text = get_search_query(chat_completion=chat_completion, user_query=text) #"eye_exam"
 
         # STEP 3) Retrieve documents from AI Search using the optimized query
         search_results = search_client.search(
@@ -190,29 +180,26 @@ def main():
                         score=doc.get("@search.score"),
                         highlight=doc.get("@search.highlights")
                         # need to retrieve citation too
+                        # TODO: when creating search resource, need to enable "metadata_storage_name" to be able to retrieve citations
                     )
                 )
-        print("DOC HIGHLIGHT", [e for d in docs for e in d.highlight["content"]]) 
-
 
         # STEP 4) Create content-specific answer using the search results and chat history
         # create messages to send to OpenAI model to generate the response
-        # TODO: uncomment once search results have been retrieved
         messages = build_messages(
             model=model_name,
             system_prompt=system_message,
             past_messages= [] if q == 0 else messages[:-1],
-            new_user_content=text + " ".join([e for d in docs for e in d.highlight["content"]]),
+            new_user_content=f"**{query_text}**" + " ".join([e for d in docs for e in d.highlight["content"]]),
             max_tokens=model_token_limit - 1024,
         )
     
-
         # create and print response
         chat_reply = oai_client.chat.completions.create(
             model=deployment_name,
             messages=messages,
             temperature=temperature,
-            # max_tokens=max_tokens,
+            max_tokens=max_tokens,
             n=1,
         )
         display_chat = chat_reply.choices[0].message.content + "\n"
@@ -221,7 +208,7 @@ def main():
         # # TODO: double-check the object type of chat_reply and whether it makes sense to append to messages
         # messages.append(chat_reply)
         messages = []
-        messages.append("")
+        messages.append(messages)
         q += 1
     
     print("Thanks for chatting! Goodbye")
